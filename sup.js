b8c1dd814987b0e9a91b491bffec4861bbb0d67a
William Lennartsson <william.j.lennartsson@gmail.com>
2019-02-04 12:11:21 +0100

    a

--- sup.js
+++ sup.js
@@ -1,2 +1,17 @@
-var hej = 10;
-//console.log(document.getElementsByClassName("item"));
+console.log("hallej");
+const items = document.getElementsByClassName("item")
+console.log(items)
+for (let i = 0; i < items.length; i++){
+    //console.log(items[i])
+    items[i].addEventListener("click", (e) => {
+        console.log(e + " din lilla räka")
+        
+    })
+}
+const items2 = document.getElementsByClassName("item2");
+
+for (let i = 0; i < items.length; i++){
+    //console.log(items[i])
+    items[i].addEventListener("click", (e) => {
+    })
+}
